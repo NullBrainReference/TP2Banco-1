13691ad74521fd1776ddd15ca8b80203dd0b4174
ghami20 <ghamiboxbox@gmail.com>
2024-11-14 09:09:10 -0300

    Cambio

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import javax.swing.JOptionPane;
 
 public class Main {
-
 	public static void main(String[] args) {
 		
-		JOptionPane.showMessageDialog(null, "Ho0la, soy ghami todavia no se programar");
-		JOptionPane.showMessageDialog(null, args);
+		 Usuario.getUsuarios();
+		
 	}
 }
